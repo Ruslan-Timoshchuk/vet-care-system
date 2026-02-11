755221694f5701a55c1ecc33f474588014a5c3b7
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-02-11 20:06:32 +0100

    security configuration adjustments

--- src/main/java/com/system/vetcare/config/SecurityConfig.java
+++ src/main/java/com/system/vetcare/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.system.vetcare.config;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static org.springframework.http.HttpMethod.*;
+import static java.util.stream.Collectors.toList;
+import java.util.Set;
+import java.util.stream.Stream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
@@ -14,30 +17,34 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-
 import com.system.vetcare.controller.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
  
+    public static final Set<String> ALLOWED_AUTHENTICATION_API_POST = Set.of(
+            "/api/v1/security/login", 
+            "/api/v1/security/registration", 
+            "/api/v1/security/refresh",
+            "/api/v1/security/validate_email");
+    public static final Set<String> ALLOWED_AUTHORITIES_API_GET = Set.of("/api/v1/authorities/all");
+    
     @Bean
-    public SecurityFilterChain filterChain(final HttpSecurity http,
-            final JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
-        http.csrf().disable()
+    public SecurityFilterChain filterChain(
+            final HttpSecurity httpSecurity,
+            final JwtAuthenticationFilter jwtAuthenticationFilter,
+            final RequestMatcher publicEndpointsMatcher) throws Exception {
+        httpSecurity.csrf().disable()
           .sessionManagement().sessionCreationPolicy(STATELESS)
                    .and()
-                   .authorizeHttpRequests()
-                   .antMatchers("/api/v1/security/login").permitAll()
-                   .antMatchers("/api/v1/security/registration").permitAll()
-                   .antMatchers("/api/v1/security/refresh").permitAll()
-                   .antMatchers("/api/v1/security/validate_email").permitAll()
-                   .antMatchers("/api/v1/authorities/all").permitAll()
+                   .authorizeRequests()
+                   .requestMatchers(publicEndpointsMatcher).permitAll()
                    .anyRequest()
                    .authenticated()
                    .and()
                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+        return httpSecurity.build();
     }
     
     @Bean
@@ -51,13 +58,14 @@ public class SecurityConfig {
     }
     
     @Bean
-    public RequestMatcher requestMatcher() {
+    public RequestMatcher publicEndpointsMatcher() {
         return new OrRequestMatcher(
-                new AntPathRequestMatcher("/api/v1/security/login", HttpMethod.POST.name()),
-                new AntPathRequestMatcher("/api/v1/security/registration", HttpMethod.POST.name()),
-                new AntPathRequestMatcher("/api/v1/security/refresh", HttpMethod.POST.name()),
-                new AntPathRequestMatcher("/api/v1/security/validate_email", HttpMethod.POST.name()),
-                new AntPathRequestMatcher("/api/v1/authorities/all", HttpMethod.POST.name()));
+                Stream
+                 .concat(ALLOWED_AUTHENTICATION_API_POST.stream()
+                          .map(url -> new AntPathRequestMatcher(url, POST.name())),
+                        ALLOWED_AUTHORITIES_API_GET.stream()
+                          .map(url -> new AntPathRequestMatcher(url, GET.name())))
+                .collect(toList()));
     }
 
 }
